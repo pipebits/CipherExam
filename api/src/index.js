143dae7c48661515fc754232bbc42b3c779c4cc0
pipebits <70990102+pipebits@users.noreply.github.com>
2023-06-12 23:31:55 -0600

    Added mongodb connection

--- api/src/index.js
+++ api/src/index.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 const express = require("express")
 const app = express()
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URL)
+
 app.listen(process.env.PORT, _ => {
     console.log(`App listening on port ${process.env.PORT}`)
 })
