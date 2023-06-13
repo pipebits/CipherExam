7c050f7fa0c788a569d86e564be703b1c3e1489b
pipebits <70990102+pipebits@users.noreply.github.com>
2023-06-12 23:32:20 -0600

    Added cors middleware

--- api/src/index.js
+++ api/src/index.js
@@ -6,6 +6,15 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json())
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+});
+
 app.listen(process.env.PORT, _ => {
     console.log(`App listening on port ${process.env.PORT}`)
 })
