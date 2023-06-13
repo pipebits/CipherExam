dde8615db54f4a2fc99ee334af69ab354e8c1f16
pipebits <70990102+pipebits@users.noreply.github.com>
2023-06-13 16:25:44 -0600

    Added user controller

---
+++ api/src/controllers/userController.js
@@ -0,0 +1,9 @@
+module.exports = {
+    create: (req, res, next) => { },
+    getOwn: (req, res, next) => { },
+    deleteOwn: (req, res, next) => { },
+    get: (req, res, next) => { },
+    delete: (req, res, next) => { },
+    login: (req, res, next) => { },
+    logout: (req, res, next) => { }
+}
