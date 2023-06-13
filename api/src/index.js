304ca955bbf528d838b363b1a2be23e9d4a42c4c
pipebits <70990102+pipebits@users.noreply.github.com>
2023-06-12 22:57:16 -0600

    Changed port configuration via .env

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require("express")
 const app = express()
 
-app.listen(3000, _ => {
-    console.log(`App listening on port 3000`)
+app.listen(process.env.PORT, _ => {
+    console.log(`App listening on port ${process.env.PORT}`)
 })
